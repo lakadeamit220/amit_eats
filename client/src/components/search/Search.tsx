72097002d3f6b0257e7191005067ad280c0e50fb
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-05-09 22:11:25 +0530

    implemented search page

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -3,13 +3,18 @@ import Filter from "../filter/Filter";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
-import { X } from "lucide-react";
+import { Globe, MapPin, X } from "lucide-react";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { AspectRatio } from "../ui/aspect-ratio";
+import hero from "@/assets/hero_pizza.png";
+import { Link } from "react-router-dom";
+
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
   return <>
     <div className="max-w-7xl mx-auto my-10">
-      <div className="flex flex-col md:flex-row justify-between gap-10 border-5">
+      <div className="flex flex-col md:flex-row justify-between gap-10">
         <Filter />
         <div className="flex-1">
           <div className="flex item-center gap-2">
@@ -37,6 +42,64 @@ const Search = () => {
               </div>
             </div>
             <div className="grid md:grid-cols-3 gap-4">
+              {
+                [1, 2, 3].map((item: number, idx: number) => (
+                  <Card className="bg-white dark:bg-gray-800 shadow-xl rounded-xl overflow-hidden hover:shadow-2xl transition-shadow duration-300">
+                    <div className="relative">
+                      <AspectRatio ratio={16 / 6}><img src={hero} alt="" className="w-full h-full object-cover" /></AspectRatio>
+                      <div className="absolute top-2 left-2 bg-white dark:bg-gray-700 opacity-75 rounded-lg px-3 py-1">
+                        <span className="text-sm font-medium text-gray-700 dark:text-gray-300">Featured</span>
+                      </div>
+                    </div>
+                    <CardContent className="p-4">
+                      <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
+                        {/* {restaurant.restaurantName} */} Tandoor Tadaka
+                      </h1>
+                      <div className="mt-2 gap-1 flex items-center text-gray-600 dark:text-gray-400">
+                        <MapPin size={16} />
+                        <p className="text-sm">
+                          City:{" "}
+                          <span className="font-medium">
+                            {/* {restaurant.city} */}
+                            Pune
+                          </span>
+                        </p>
+                      </div>
+                      <div className="mt-2 gap-1 flex items-center text-gray-600 dark:text-gray-400">
+                        <Globe size={16} />
+                        <p className="text-sm">
+                          Country:{" "}
+                          <span className="font-medium">
+                            {/* {restaurant.country} */}
+                            India
+                          </span>
+                        </p>
+                      </div>
+                      <div className="flex gap-2 mt-4 flex-wrap">
+                        {/* {restaurant.cuisines.map( */}
+                        {/* (cuisine: string, idx: number) => ( */}
+                        <Badge
+                          // key={idx}
+                          className="font-medium px-2 py-1 rounded-full shadow-sm"
+                        >
+                          {/* {cuisine} */} Paneer
+                        </Badge>
+                        {/* ) */}
+                        {/* )} */}
+                      </div>
+                    </CardContent>
+                    <CardFooter className="p-4 border-t dark:border-t-gray-700 border-t-gray-100 text-white flex justify-end">
+                      <Link to="/home"
+                      // {`/restaurant/${restaurant._id}`}
+                      >
+                        <Button className="bg-[#D19254] hover:bg-[#D18C47] font-semibold py-2 px-4 rounded-full shadow-md transition-colors duration-200">
+                          View Menus
+                        </Button>
+                      </Link>
+                    </CardFooter>
+                  </Card>
+                ))
+              }
 
             </div>
           </div>
